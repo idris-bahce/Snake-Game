82738f696f0d911314e2b493075875eedf6432f2
idris <idrisbahce26@gmail.com>
2023-04-07 03:57:37 +0300

    Snake Game

---
+++ snake_game/snake.py
@@ -0,0 +1,57 @@
+from turtle import Turtle
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.penup()
+        new_segment.color("white")
+        new_segment.setpos(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for i in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[i - 1].xcor()
+            new_y = self.segments[i - 1].ycor()
+            self.segments[i].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() == 270:
+            pass
+        else:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() == 90:
+            pass
+        else:
+            self.head.setheading(270)
+
+    def right(self):
+        if self.head.heading() == 180:
+            pass
+        else:
+            self.head.setheading(0)
+
+    def left(self):
+        if self.head.heading() == 0:
+            pass
+        else:
+            self.head.setheading(180)
